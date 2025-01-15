2193e28795b509af02d9faecf2c3e429d8863b6e
Jupp Szepaniak <sze.jupp2001@gmail.com>
2025-01-15 20:55:43 +0100

    trial and error

--- src/script.js
+++ src/script.js
@@ -1,12 +1,11 @@
-const board = document.getElementsByClassName("board");
-const squares = document.getElementsByClassName("square");
+const squares = document.querySelectorAll(".square");
 const stateDisplay = document.querySelector("#state");
 const infoDisplay = document.querySelector("#info");
 
-const players = ["X", "O"];
+let counter = 0;
 let curPlayer = "X";
 
-const startCells = [["", "", "", "", "", "", "", "", ""]];
+let gameStatus = [["", "", "", "", "", "", "", "", ""]];
 
 const winningCombination = [
   [1, 1, 1, 0, 0, 0, 0, 0, 0],
@@ -19,11 +18,42 @@ const winningCombination = [
   [0, 0, 1, 0, 1, 0, 1, 0, 0],
 ];
 
-infoDisplay.textContent = "X ... ist an der Reihe.";
+infoDisplay.textContent = "X ist an der Reihe.";
 stateDisplay.textContent = "Das Spiel beginnt mit dem ersten Zug von X...";
 
-function checkUserOption() {
-  console.log("The id of the element you clicked: " + squares.id);
+squares.forEach((square) => {
+  square.addEventListener("click", () => {
+    // Überprüfe, ob die Kachel schon belegt ist
+    if (!square.textContent && curPlayer === "X") {
+      square.textContent = "X";
+      curPlayer = "O";
+      infoDisplay.textContent = "O ist an der Reihe.";
+      checkWinningCombination();
+    } else if (!square.textContent && curPlayer === "O") {
+      square.textContent = "O";
+      curPlayer = "X";
+      infoDisplay.textContent = "X ist an der Reihe.";
+      checkWinningCombination();
+    }
+    counter++;
+    console.log(counter);
+  });
+});
+
+function checkWinningCombination() {
+  if (winningCombination == gameStatus && curPlayer === "X" && counter === 8) {
+    infoDisplay.textContent = "Game Over.";
+    stateDisplay.textContent = "X hat gewonnen.";
+  } else if (winningCombination == gameStatus && curPlayer === "O" && counter === 8) {
+    infoDisplay.textContent = "Game Over.";
+    stateDisplay.textContent = "O hat gewonnen.";
+  }
 }
 
-function resetGame() {}
+function resetGame() {
+  squares.forEach((square) => {
+    square.textContent = "";
+    curPlayer = "X";
+    infoDisplay.textContent = "X ist an der Reihe.";
+  });
+}
